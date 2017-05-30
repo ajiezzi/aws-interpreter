4f0e92e7f8b3021bf845c093b14174e524c14748
Adam Iezzi <adam.iezzi@gmail.com>
2017-05-29 20:35:01 -0400

    updated bootstrap config

--- aws-deployment/bootstrap_base.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Copy AWS interpreter dependencies
-aws s3 cp s3://zeppelin-emr-config/aws-interpreter-0.0.1-SNAPSHOT.jar .
-aws s3 cp s3://zeppelin-emr-config/commons-exec-1.3.jar .
